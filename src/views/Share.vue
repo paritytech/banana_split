d0d555f061fbff615ac4266e4781ca9778d459db
Kirill Pimenov <kirill@parity.io>
2018-11-18 17:37:16 +0100

    Variable number of shares

--- src/views/Share.vue
+++ src/views/Share.vue
@@ -2,6 +2,8 @@
 <div>
     <h1>Share secrets</h1>
     <textarea v-model="secret" placeholder="Your secret goes here"></textarea>
+    <p><input type="range" v-model.number="totalShards" v-bind:min="requiredShards" max="16"/>{{totalShards}}</p>
+    <p><input type="range" v-model.number="requiredShards" min="1" v-bind:max="totalShards"/>{{requiredShards}}</p>
     <p>{{shards}}</p>
 </div>
 </template>
@@ -13,7 +15,9 @@ export default {
     name: 'Share',
     data: function () {
         return {
-            secret: ''
+            secret: '',
+            totalShards: 5,
+            requiredShards: 3
         }
     },
     computed: {
@@ -21,7 +25,7 @@ export default {
             if (this.secret === '') {
                 return []
             }
-            return SECRETS.share(SECRETS.str2hex(this.secret), 5, 3)
+            return SECRETS.share(SECRETS.str2hex(this.secret), this.totalShards, this.requiredShards)
         }
     }
 }
