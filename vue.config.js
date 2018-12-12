faffc0475d22c7dfa681ce2558ec4de3508df14d
Kirill Pimenov <kirill@parity.io>
2018-12-12 13:52:32 +0300

    Make webpack build a self-contained html page
    Thanks to https://forum.vuejs.org/u/Jayesh

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
 module.exports = {
   productionSourceMap: false,
   css: {
     extract: false
+  },
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        inlineSource: '.(js|css)$'
+      }),
+      new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin)
+    ]
   }
 }
