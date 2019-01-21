90d5e6b82b3ec7457ee68e6fead08f1916cf3b63
Kirill Pimenov <kirill@pimenov.cc>
2019-01-21 19:35:20 +0300

    Install JUnit reporter for Jest

--- jest.config.js
+++ jest.config.js
@@ -19,5 +19,6 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://localhost/'
+  testURL: 'http://localhost/',
+  reporters: [ 'default', 'jest-junit' ]
 }
