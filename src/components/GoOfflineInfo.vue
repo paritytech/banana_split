c690d45432cfeb7dce69de2599c090cf21f90c4d
Alex Van de Sande <avsa@ethereum.org>
2020-06-11 13:56:15 -0300

    added warning styles

--- src/components/GoOfflineInfo.vue
+++ src/components/GoOfflineInfo.vue
@@ -1,22 +1,35 @@
 <template>
+  <div class="warning">
+    <h1>Please go offline, so your secrets won't leak accidentally</h1>
     <div>
-      <h1>Please go offline, so your secrets won't leak accidentally</h1>
-      <div>
-        <p>This application doesn't require Internet access, and you shouldn't be using it from a brower which has one.</p>
-        <p>It's really trivial to accidentally upload your unencrypted secrets somewhere, with a help of your browser spellchecker, webpage translation extension and such.</p>
-        <p>In Firefox, please go to Menu→More→Work Offline.</p>
-        <p>In Chrome, you'll have to open DevTools (Ctrl+Shift+I), then in the Network tab there check the "Offline" checkbox. (DON'T CLOSE DEVELOPER TOOLS AFTER THAT!)</p>
-      </div>
+      <p>
+        This application doesn't require Internet access, and you shouldn't be
+        using it from a brower which has one.
+      </p>
+      <p>
+        It's really trivial to accidentally upload your unencrypted secrets
+        somewhere, with a help of your browser spellchecker, webpage translation
+        extension and such.
+      </p>
+      <p>In Firefox, please go to Menu→More→Work Offline.</p>
+      <p>
+        In Chrome, you'll have to open DevTools (Ctrl+Shift+I), then in the
+        Network tab there check the "Offline" checkbox. (DON'T CLOSE DEVELOPER
+        TOOLS AFTER THAT!)
+      </p>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'GoOfflineInfo'
-
-}
+  name: "GoOfflineInfo"
+};
 </script>
 
 <style>
-
+.warning {
+  background: #ffbbb3;
+  border: solid #e87f73 7px;
+}
 </style>
--- src/components/SavePageInfo.vue
+++ src/components/SavePageInfo.vue
@@ -1,19 +1,22 @@
 <template>
+  <div class="warning">
+    <h1>Please save this page to your local drive before use</h1>
     <div>
-      <h1>Please save this page to your local drive before use</h1>
-      <div>
-        Please <a href="." download="BananaSplit.html">save this html file</a> to a folder on your local drive,
-        and then open the file from there.
-        </div>
+      Please <a href="." download="BananaSplit.html">save this html file</a> to
+      a folder on your local drive, and then open the file from there.
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'SavePageInfo'
-}
+  name: "SavePageInfo"
+};
 </script>
 
 <style>
-
+.warning {
+  background: #ffbbb3;
+  border: solid #e87f73 7px;
+}
 </style>
