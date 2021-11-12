9284b7c3ad54f14fd9c7b35c6c3b435f603b6b4a
Pavel Rybalko <pavel@parity.io>
2021-11-12 21:05:12 +0700

    prettify

--- tests/e2e/index.spec.ts
+++ tests/e2e/index.spec.ts
@@ -2,38 +2,32 @@ import { test, expect } from "@playwright/test";
 import { chromium } from "playwright";
 import { makeVideoFromImages, scaleImages } from "./util/ffmpeg";
 
-const SECRET = "top secret";
-
-const IMG_PREFIX = "qr_";
-const IMAGES = `${IMG_PREFIX}%01d.png`;
-const VIDEO = "fake-camera.y4m";
-
 test("happy path", async ({ page, baseURL }, testInfo) => {
+  const secret = "top secret";
+  const images = `%01d.png`;
+  const video = "fake-camera.y4m";
+
   await test.step("create QR codes", async () => {
     await page.goto(`${baseURL}`);
     await page.click("#shareNav");
     await page.fill("#secretTitle", "Title");
-    await page.fill("#secret", SECRET);
+    await page.fill("#secret", secret);
     await page.click("#generateBtn");
 
     const qrCodes = page.locator("#print .qr-tile .card-qr canvas");
     await expect(qrCodes).toHaveCount(3);
-    await qrCodes
-      .first()
-      .screenshot({ path: testInfo.outputPath(`${IMG_PREFIX}1.png`) });
-    await qrCodes
-      .last()
-      .screenshot({ path: testInfo.outputPath(`${IMG_PREFIX}2.png`) });
+    await qrCodes.first().screenshot({ path: testInfo.outputPath("1.png") });
+    await qrCodes.last().screenshot({ path: testInfo.outputPath("2.png") });
   });
 
   await test.step("make qr code images the same size", async () => {
-    await scaleImages(testInfo.outputPath(IMAGES));
+    await scaleImages(testInfo.outputPath(images));
   });
 
   await test.step("make video from qr codes", async () => {
     await makeVideoFromImages(
-      testInfo.outputPath(IMAGES),
-      testInfo.outputPath(VIDEO)
+      testInfo.outputPath(images),
+      testInfo.outputPath(video)
     );
   });
 
@@ -42,8 +36,8 @@ test("happy path", async ({ page, baseURL }, testInfo) => {
       args: [
         "--use-fake-ui-for-media-stream",
         "--use-fake-device-for-media-stream",
-        `--use-file-for-fake-video-capture=${testInfo.outputPath(VIDEO)}`
-      ]
+        `--use-file-for-fake-video-capture=${testInfo.outputPath(video)}`,
+      ],
     });
 
     const page = await browser.newPage();
@@ -52,6 +46,6 @@ test("happy path", async ({ page, baseURL }, testInfo) => {
     await page.waitForSelector("#passphrase");
     await page.fill("#passphrase", "TEST");
     await page.click("#reconstructBtn");
-    await expect(page.locator("#recoveredSecret")).toHaveValue(SECRET);
+    await expect(page.locator("#recoveredSecret")).toHaveValue(secret);
   });
 });
